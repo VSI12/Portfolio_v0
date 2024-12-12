c240dc0b790cacdabf0340aa527a3bfca5ff7ec7
Victor Iliya <VictorIliya15@gmail.com>
2024-12-13 00:18:44 +0100

    Discord link

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -18,7 +18,7 @@ const Socials = () => {
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
           width="24px" fill="#525252" className={styles.icon}><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>
       </a>
-      <a href="https://www.linkedin.com/in/victor-iliya/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.discordapp.com/users/1147212408490836028/" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faDiscord}  className={styles.icon} />
       </a>
       <a href="https://www.linkedin.com/in/victor-iliya/" target="_blank" rel="noopener noreferrer">
